e0a62fd49bdbe311b718694cf6be7548ac8684fa
Dunne7 <D00288376@student.dkit.ie>
2026-03-13 10:14:45 +0000

    added excel file

--- tests/pytest_test_function.py
+++ tests/pytest_test_function.py
@@ -1,6 +1,9 @@
 import pytest
 from pytest_pract2 import fahrenheit_to_celsius
 
+
+"""Q1"""
+
 def convert_to_celsius(fahrenheit: float):
     return fahrenheit(30, 0)
 
@@ -11,8 +14,32 @@ def test_fahrenheit_to_celsius(farenheit):
 #def test_get_celsius(fahrenheit):
 #    assert test_get_celsius(fahrenheit) == expected
 
+"""Q 2"""
+#created test for reverse string 
+#to confirm text outputted when unreversed
 def get_rev_string(s: str): 
     return ("olleh","hello")
     return ("kcab", "back")
 
+#param
+
+"""Q3"""
+def add(a,b):
+    return(5,10 == 15)
+    return(10,5 == 15)
+    return(9,4 == 12)
+
+#param
+
+"""Q4"""
+def student_record(line: str):
+    return 
+    ("John", "40", "Pass")
+    ("Alan", "60", "2.1")
+    ("Adrian", "89", "First Class")
+    ("Alex", "55", "2.2")
+
+@pytest.mark.parametrize()
+
+    
 
