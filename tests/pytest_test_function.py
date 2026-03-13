09075ac6f01554d8b3289d52280d4a8713ae477d
Dunne7 <D00288376@student.dkit.ie>
2026-03-13 09:50:11 +0000

    updated test functions

--- tests/pytest_test_function.py
+++ tests/pytest_test_function.py
@@ -2,4 +2,17 @@ import pytest
 from pytest_pract2 import fahrenheit_to_celsius
 
 def convert_to_celsius(fahrenheit: float):
-    return fahrenheit
+    return fahrenheit(30, 0)
+
+def test_fahrenheit_to_celsius(farenheit):
+    assert fahrenheit_to_celsius(30) == 0
+
+#@pytest.mark.parameterize("fahrenheit, expected",[(30, 0)])
+#def test_get_celsius(fahrenheit):
+#    assert test_get_celsius(fahrenheit) == expected
+
+def get_rev_string(s: str): 
+    return ("olleh","hello")
+    return ("kcab", "back")
+
+
